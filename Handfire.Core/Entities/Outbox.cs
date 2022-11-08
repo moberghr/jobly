e51b2c94356f2ba491c4757d08cb9b79b0c1705a
Domagoj Medo <domagoj.medo@moberg.hr>
2022-11-08 15:42:37 +0100

    Add project files.

---
+++ Handfire.Core/Entities/Outbox.cs
@@ -0,0 +1,14 @@
+﻿namespace Handfire.Core.Entities;
+
+public class OutboxMessage
+{
+    public int Id { get; set; }
+
+    public string Type { get; set; }
+
+    public string Message { get; set; }
+
+    public DateTime CreateTime { get; set; }
+
+    public DateTime? ProcessedTime { get; set; }
+}
---
+++ Handfire.Core/HandfireContext.cs
@@ -0,0 +1,14 @@
+﻿using Handfire.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Handfire.Core;
+
+public class HandfireContext : DbContext
+{
+    public HandfireContext(DbContextOptions<HandfireContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<OutboxMessage> OutboxMessages => Set<OutboxMessage>();
+}
