a4e590bb04f0d15f77108873db65d76a13f11105
Zvonimir Sorić <zvonimir.soric@moberg.hr>
2023-01-19 14:29:29 +0100

    Test

---
+++ Handfire.Tests/TestData/SqlServerTestBase.cs
@@ -0,0 +1,53 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using Handfire.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Handfire.Tests.TestData;
+public class SqlServerTestBase : IAsyncLifetime
+{
+    private readonly MsSqlTestcontainer _dbContainer = new TestcontainersBuilder<MsSqlTestcontainer>()
+        .WithDatabase(
+            new MsSqlTestcontainerConfiguration
+            {
+                Database = "Handfire",
+                Password = Guid.NewGuid().ToString("D"),
+            })
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .WithEnvironment("ACCEPT_EULA", "Y")
+        .WithCleanUp(true)
+        .Build();
+
+    public TContext CreateTContext<TContext>() where TContext : DbContext
+    {
+        var testContext = new TestContext(new DbContextOptionsBuilder<TestContext>()
+           .UseSqlServer(_dbContainer.ConnectionString + ";Encrypt=False;")
+           .Options);
+
+        testContext.Database.EnsureCreated();
+
+        return testContext as TContext;
+    }
+
+    public TestContext CreateContext()
+    {
+        var testContext = new TestContext(new DbContextOptionsBuilder<TestContext>()
+           .UseSqlServer(_dbContainer.ConnectionString + ";Encrypt=False;")
+           .Options);
+
+        testContext.Database.EnsureCreated();
+
+        return testContext;
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _dbContainer.DisposeAsync();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+    }
+}
