076468e8ce18f3feabdeca57f2255c8e87c7cbc3
Domagoj Medo <domagoj.medo@moberg.hr>
2023-09-13 14:14:08 +0200

    /dashboard works

---
+++ Handfire.Core/UIMiddleware/HandfireUIMiddleware.cs
@@ -0,0 +1,108 @@
+﻿using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Handfire.Core
+{
+    public class HandfireUIMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly HandfireUIOptions _options;
+        private readonly StaticFileMiddleware _staticFileMiddleware;
+        private const string EmbeddedFileNamespace = "Handfire.Core.UI";
+
+        public HandfireUIMiddleware(RequestDelegate next, IWebHostEnvironment hostingEnv, ILoggerFactory loggerFactory, HandfireUIOptions options)
+        {
+            _next = next;
+            _options = options ?? new HandfireUIOptions();
+            _staticFileMiddleware = CreateStaticFileMiddleware(next, hostingEnv, loggerFactory, _options);
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var httpMethod = httpContext.Request.Method;
+            var path = httpContext.Request.Path.Value!;
+
+            var extension = Path.GetExtension(path);
+
+            if (!string.IsNullOrWhiteSpace(extension))
+            {
+                await _staticFileMiddleware.Invoke(httpContext);
+
+                return;
+            }
+
+            if (httpMethod == HttpMethod.Get.Method && path.StartsWith(_options.RoutePrefix) && !path.Contains("/api/"))
+            {
+                await RespondWithIndexHtml(httpContext.Response);
+
+                return;
+            }
+
+            //if (httpMethod == "GET" && Regex.IsMatch(path, $"^/?{Regex.Escape(_options.RoutePrefix)}/?$", RegexOptions.IgnoreCase))
+            //{
+            //    // Use relative redirect to support proxy environments
+            //    var relativeIndexUrl = string.IsNullOrEmpty(path) || path.EndsWith("/")
+            //        ? "index.html"
+            //        : $"{path.Split('/').Last()}/index.html";
+
+            //    RespondWithRedirect(httpContext.Response, relativeIndexUrl);
+
+            //    return;
+            //}
+
+            //if (httpMethod == "GET" && Regex.IsMatch(path, $"^/{Regex.Escape(_options.RoutePrefix)}/?index.html$", RegexOptions.IgnoreCase))
+            //{
+            //    await RespondWithIndexHtml(httpContext.Response);
+
+            //    return;
+            //}
+
+            await _next(httpContext);
+        }
+
+        public static StaticFileMiddleware CreateStaticFileMiddleware(RequestDelegate next, IWebHostEnvironment hostingEnv, ILoggerFactory loggerFactory, HandfireUIOptions _options)
+        {
+            var staticFileOptions = new StaticFileOptions
+            {
+                RequestPath = _options.RoutePrefix,
+                FileProvider = new EmbeddedFileProvider(typeof(HandfireUIMiddleware).GetTypeInfo().Assembly, EmbeddedFileNamespace),
+            };
+
+            return new StaticFileMiddleware(next, hostingEnv, Options.Create(staticFileOptions), loggerFactory);
+        }
+
+        private void RespondWithRedirect(HttpResponse response, string location)
+        {
+            response.StatusCode = 301;
+            response.Headers["Location"] = location;
+        }
+
+        private async Task RespondWithIndexHtml(HttpResponse response)
+        {
+            response.StatusCode = 200;
+            response.ContentType = "text/html;charset=utf-8";
+
+            using var stream = _options.IndexStream();
+            using var reader = new StreamReader(stream);
+
+            // Inject arguments before writing to response
+            var htmlBuilder = new StringBuilder(await reader.ReadToEndAsync());
+
+            var htmlString = htmlBuilder.ToString();
+
+            htmlString = htmlString.Replace("href=\"static", $"href=\"{_options.RoutePrefix}/static");
+            htmlString = htmlString.Replace("href=\"favicon", $"href=\"{_options.RoutePrefix}/favicon");
+            htmlString = htmlString.Replace("src=\"static", $"src=\"{_options.RoutePrefix}/static");
+
+            await response.WriteAsync(htmlString, Encoding.UTF8);
+        }
+
+    }
+}
