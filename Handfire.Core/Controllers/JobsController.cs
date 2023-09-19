794682e731a6bf375e9054522ebbd5288ac32e4b
VidraTo <39879137+TommyVidra@users.noreply.github.com>
2023-09-19 16:06:40 +0200

    indent fix

--- Handfire.Core/Controllers/JobsController.cs
+++ Handfire.Core/Controllers/JobsController.cs
@@ -9,50 +9,50 @@ namespace Handfire.Core.Controllers;
 [ApiExplorerSettings(IgnoreApi = true)]
 public class JobsController : Controller
 {
-	private readonly IHandfireService _handfireService;
+    private readonly IHandfireService _handfireService;
 
-	public JobsController(IHandfireService handfireService)
-	{
-		_handfireService = handfireService;
-	}
+    public JobsController(IHandfireService handfireService)
+    {
+        _handfireService = handfireService;
+    }
 
-	[HttpGet("created")]
-	public async Task<IActionResult> Created(BaseListRequest request)
-	{
-		var model = await _handfireService.GetJobsList(request, State.Enqueued);
+    [HttpGet("created")]
+    public async Task<IActionResult> Created(BaseListRequest request)
+    {
+        var model = await _handfireService.GetJobsList(request, State.Enqueued);
 
-		return View(model);
-	}
+        return View(model);
+    }
 
-	[HttpGet("completed")]
-	public async Task<IActionResult> Completed(BaseListRequest request)
-	{
-		var model = await _handfireService.GetJobsList(request, State.Completed);
+    [HttpGet("completed")]
+    public async Task<IActionResult> Completed(BaseListRequest request)
+    {
+        var model = await _handfireService.GetJobsList(request, State.Completed);
 
-		return View(model);
-	}
+        return View(model);
+    }
 
-	[HttpGet("failed")]
-	public async Task<IActionResult> Failed(BaseListRequest request)
-	{
-		var model = await _handfireService.GetJobsList(request, State.Failed);
+    [HttpGet("failed")]
+    public async Task<IActionResult> Failed(BaseListRequest request)
+    {
+        var model = await _handfireService.GetJobsList(request, State.Failed);
 
-		return View(model);
-	}
-	[HttpGet("processing")]
-	public async Task<IActionResult> Processing(BaseListRequest request)
-	{
-		var model = await _handfireService.GetJobStatesInProcess(request);
-		return View(model);
-	}
+        return View(model);
+    }
+    [HttpGet("processing")]
+    public async Task<IActionResult> Processing(BaseListRequest request)
+    {
+        var model = await _handfireService.GetJobStatesInProcess(request);
+        return View(model);
+    }
 
-	[HttpGet("retry")]
-	public async Task<IActionResult> Retry(string jobId)
-	{
-		await _handfireService.SetRetry(jobId);
+    [HttpGet("retry")]
+    public async Task<IActionResult> Retry(string jobId)
+    {
+        await _handfireService.SetRetry(jobId);
 
-		var url = Request.GetTypedHeaders().Referer!.ToString();
+        var url = Request.GetTypedHeaders().Referer!.ToString();
 
-		return Redirect(url);
-	}
+        return Redirect(url);
+    }
 }
--- Handfire.Core/Worker/HandfireWorkerService.cs
+++ Handfire.Core/Worker/HandfireWorkerService.cs
@@ -13,282 +13,282 @@ namespace Handfire.Core;
 
 public interface IHandfireWorkerService
 {
-	Task GetAndProcessJob(CancellationToken cancellationToken);
+    Task GetAndProcessJob(CancellationToken cancellationToken);
 }
 
 public class HandfireWorkerService<TContext> : IHandfireWorkerService
-	where TContext : DbContext
+    where TContext : DbContext
 {
-	private readonly string _workerId = Guid.NewGuid().ToString();
-	private readonly IServiceScopeFactory _serviceScopeFactory;
-	private readonly ILogger<HandfireWorkerService<TContext>> _logger;
+    private readonly string _workerId = Guid.NewGuid().ToString();
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly ILogger<HandfireWorkerService<TContext>> _logger;
 
-	public HandfireWorkerService(IServiceScopeFactory serviceScopeFactory, ILogger<HandfireWorkerService<TContext>> logger)
-	{
-		_serviceScopeFactory = serviceScopeFactory;
-		_logger = logger;
-	}
+    public HandfireWorkerService(IServiceScopeFactory serviceScopeFactory, ILogger<HandfireWorkerService<TContext>> logger)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+        _logger = logger;
+    }
 
-	public async Task UpdateJobStatus(Job job, CancellationToken cancellationToken)
-	{
-		using var scope = _serviceScopeFactory.CreateScope();
-		var context = scope.ServiceProvider.GetRequiredService<TContext>();
+    public async Task UpdateJobStatus(Job job, CancellationToken cancellationToken)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<TContext>();
 
-		var jobState = new JobState
-		{
-			JobId = job.Id,
-			DateTime = DateTime.UtcNow,
-			State = State.Processing,
-			Message = $"The job {job.Id} is being processed"
-		};
+        var jobState = new JobState
+        {
+            JobId = job.Id,
+            DateTime = DateTime.UtcNow,
+            State = State.Processing,
+            Message = $"The job {job.Id} is being processed"
+        };
 
-		await context.Set<JobState>().AddAsync(jobState, cancellationToken);
-		await context.SaveChangesAsync(cancellationToken);
+        await context.Set<JobState>().AddAsync(jobState, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
 
-		context.SaveChanges();
-	}
+        context.SaveChanges();
+    }
 
-	public async Task GetAndProcessJob(CancellationToken cancellationToken)
-	{
-		using var scope = _serviceScopeFactory.CreateScope();
+    public async Task GetAndProcessJob(CancellationToken cancellationToken)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
 
-		var context = scope.ServiceProvider.GetRequiredService<TContext>();
+        var context = scope.ServiceProvider.GetRequiredService<TContext>();
 
-		using var transaction = await context.Database.BeginTransactionAsync(cancellationToken);
+        using var transaction = await context.Database.BeginTransactionAsync(cancellationToken);
 
-		var jobData = context.Set<Job>()
-			.Where(x => x.CurrentState == State.Enqueued)
-			.Where(x => x.ScheduleTime < DateTime.UtcNow || x.ScheduleTime == null)
-			.Select(x =>
-					new JobData
-					{
-						Job = x,
-						IsParent = x.ChildJobs.Any(),
-					})
-			.TagWith(InterceptorConstants.RowLockTableJob)
-			.FirstOrDefault();
+        var jobData = context.Set<Job>()
+            .Where(x => x.CurrentState == State.Enqueued)
+            .Where(x => x.ScheduleTime < DateTime.UtcNow || x.ScheduleTime == null)
+            .Select(x =>
+                    new JobData
+                    {
+                        Job = x,
+                        IsParent = x.ChildJobs.Any(),
+                    })
+            .TagWith(InterceptorConstants.RowLockTableJob)
+            .FirstOrDefault();
 
-		var job = jobData?.Job;
+        var job = jobData?.Job;
 
-		// if we didn't find any messages then we wait, otherwise we query again immediately 
-		if (job == null)
-		{
-			await Task.Delay(1000, cancellationToken);
+        // if we didn't find any messages then we wait, otherwise we query again immediately 
+        if (job == null)
+        {
+            await Task.Delay(1000, cancellationToken);
 
-			return;
-		}
+            return;
+        }
 
-		_logger.LogInformation("Worker {workerId} fetched message {id}", _workerId, job.Id);
+        _logger.LogInformation("Worker {workerId} fetched message {id}", _workerId, job.Id);
 
-		try
-		{
-			await UpdateJobStatus(job, cancellationToken);
+        try
+        {
+            await UpdateJobStatus(job, cancellationToken);
 
-			if (job.RecurringJobId.HasValue)
-			{
-				await CreateNextJob(job, cancellationToken);
-			}
+            if (job.RecurringJobId.HasValue)
+            {
+                await CreateNextJob(job, cancellationToken);
+            }
 
-			await ProcessOutboxMessage(job, cancellationToken);
-		}
-		catch (Exception e)
-		{
-			await UpdateJobData(context, jobData!, e.Message, cancellationToken);
+            await ProcessOutboxMessage(job, cancellationToken);
+        }
+        catch (Exception e)
+        {
+            await UpdateJobData(context, jobData!, e.Message, cancellationToken);
 
-			transaction.Commit();
-			return;
-		}
+            transaction.Commit();
+            return;
+        }
 
-		await UpdateJobData(context, jobData!, message: null, cancellationToken);
-		transaction.Commit();
-	}
+        await UpdateJobData(context, jobData!, message: null, cancellationToken);
+        transaction.Commit();
+    }
 
-	private async Task CreateNextJob(Job job, CancellationToken cancellationToken)
-	{
-		using var scope = _serviceScopeFactory.CreateScope();
+    private async Task CreateNextJob(Job job, CancellationToken cancellationToken)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
 
-		var temporaryContext = scope.ServiceProvider.GetRequiredService<TContext>();
+        var temporaryContext = scope.ServiceProvider.GetRequiredService<TContext>();
 
-		var recurringJob = await temporaryContext.Set<RecurringJob>()
-			.Where(x => x.Id == job.RecurringJobId)
-			.FirstAsync(cancellationToken);
+        var recurringJob = await temporaryContext.Set<RecurringJob>()
+            .Where(x => x.Id == job.RecurringJobId)
+            .FirstAsync(cancellationToken);
 
-		if (recurringJob.NextJobId != job.Id)
-		{
-			return;
-		}
+        if (recurringJob.NextJobId != job.Id)
+        {
+            return;
+        }
 
-		var createTime = DateTime.UtcNow;
+        var createTime = DateTime.UtcNow;
 
-		var fromUtc = DateTime.SpecifyKind(recurringJob.NextExecution ?? DateTime.UtcNow, DateTimeKind.Utc);
-		var nextJobScheduleTime = CronExpression.Parse(recurringJob.Cron).GetNextOccurrence(fromUtc);
+        var fromUtc = DateTime.SpecifyKind(recurringJob.NextExecution ?? DateTime.UtcNow, DateTimeKind.Utc);
+        var nextJobScheduleTime = CronExpression.Parse(recurringJob.Cron).GetNextOccurrence(fromUtc);
 
-		var jobStats = new List<JobState>
-		{
-			new() { State = State.Enqueued, DateTime = createTime}
-		};
+        var jobStats = new List<JobState>
+        {
+            new() { State = State.Enqueued, DateTime = createTime}
+        };
 
-		var newJobId = Guid.NewGuid().ToString();
-		var newJob = new Job
-		{
-			Id = newJobId,
-			Message = recurringJob.Message,
-			Type = recurringJob.Type,
-			CreateTime = createTime,
-			ScheduleTime = nextJobScheduleTime,
-			CurrentState = State.Enqueued,
-			RecurringJobId = recurringJob.Id,
-			JobStates = jobStats
-		};
+        var newJobId = Guid.NewGuid().ToString();
+        var newJob = new Job
+        {
+            Id = newJobId,
+            Message = recurringJob.Message,
+            Type = recurringJob.Type,
+            CreateTime = createTime,
+            ScheduleTime = nextJobScheduleTime,
+            CurrentState = State.Enqueued,
+            RecurringJobId = recurringJob.Id,
+            JobStates = jobStats
+        };
 
-		recurringJob.LastExecution = recurringJob.NextExecution;
-		recurringJob.LastJobId = recurringJob.NextJobId;
+        recurringJob.LastExecution = recurringJob.NextExecution;
+        recurringJob.LastJobId = recurringJob.NextJobId;
 
-		recurringJob.NextExecution = nextJobScheduleTime;
-		recurringJob.NextJob = newJob;
+        recurringJob.NextExecution = nextJobScheduleTime;
+        recurringJob.NextJob = newJob;
 
-		await temporaryContext.SaveChangesAsync(cancellationToken);
-	}
+        await temporaryContext.SaveChangesAsync(cancellationToken);
+    }
 
-	private async Task ProcessOutboxMessage(Job message, CancellationToken cancellationToken)
-	{
-		var type = Type.GetType(message.Type);
+    private async Task ProcessOutboxMessage(Job message, CancellationToken cancellationToken)
+    {
+        var type = Type.GetType(message.Type);
 
-		if (type is null)
-		{
-			throw new HandfireException($"Unknown type {message.Type}");
-		}
+        if (type is null)
+        {
+            throw new HandfireException($"Unknown type {message.Type}");
+        }
 
-		var request = JsonSerializer.Deserialize(message.Message, type);
+        var request = JsonSerializer.Deserialize(message.Message, type);
 
-		if (request is null)
-		{
-			throw new HandfireException($"Unable to deserialize message {message.Message} to type {message.Type}");
-		}
+        if (request is null)
+        {
+            throw new HandfireException($"Unable to deserialize message {message.Message} to type {message.Type}");
+        }
 
-		using var scope = _serviceScopeFactory.CreateScope();
+        using var scope = _serviceScopeFactory.CreateScope();
 
-		var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
 
-		await mediator.Send(request, cancellationToken);
-	}
+        await mediator.Send(request, cancellationToken);
+    }
 
-	private static async Task UpdateJobData(TContext context, JobData jobData, string? message, CancellationToken cancellationToken)
-	{
-		var state = !string.IsNullOrEmpty(message) ? State.Failed : State.Completed;
-		if (jobData.Job.RetriedTimes < jobData.Job.MaxRetries && !string.IsNullOrEmpty(message))
-		{
-			state = State.Enqueued;
-			jobData.Job.RetriedTimes += 1;
-		}
+    private static async Task UpdateJobData(TContext context, JobData jobData, string? message, CancellationToken cancellationToken)
+    {
+        var state = !string.IsNullOrEmpty(message) ? State.Failed : State.Completed;
+        if (jobData.Job.RetriedTimes < jobData.Job.MaxRetries && !string.IsNullOrEmpty(message))
+        {
+            state = State.Enqueued;
+            jobData.Job.RetriedTimes += 1;
+        }
 
-		jobData.Job.CurrentState = state;
+        jobData.Job.CurrentState = state;
 
-		if (jobData.Job.CurrentState == State.Completed && jobData.IsParent)
-		{
-			await UpdateChildJobs(context, jobData.Job.Id, cancellationToken);
-		}
+        if (jobData.Job.CurrentState == State.Completed && jobData.IsParent)
+        {
+            await UpdateChildJobs(context, jobData.Job.Id, cancellationToken);
+        }
 
-		if (jobData.Job.BatchId != null)
-		{
-			await UpdateCurrentAndNextBatchFromChildJob(context, jobData.Job.BatchId, cancellationToken);
-		}
+        if (jobData.Job.BatchId != null)
+        {
+            await UpdateCurrentAndNextBatchFromChildJob(context, jobData.Job.BatchId, cancellationToken);
+        }
 
-		await CreateJobState(context, jobData.Job.Id, state, string.IsNullOrEmpty(message) ? $"Job {jobData.Job.Id} is completed" : message, cancellationToken);
-	}
+        await CreateJobState(context, jobData.Job.Id, state, string.IsNullOrEmpty(message) ? $"Job {jobData.Job.Id} is completed" : message, cancellationToken);
+    }
 
-	private static async Task CreateJobState(TContext context, string jobId, State state, string? message, CancellationToken cancellationToken)
-	{
-		var jobState = new JobState
-		{
-			JobId = jobId,
-			DateTime = DateTime.UtcNow,
-			State = state,
-			Message = message
-		};
+    private static async Task CreateJobState(TContext context, string jobId, State state, string? message, CancellationToken cancellationToken)
+    {
+        var jobState = new JobState
+        {
+            JobId = jobId,
+            DateTime = DateTime.UtcNow,
+            State = state,
+            Message = message
+        };
 
-		await context.Set<JobState>().AddAsync(jobState, cancellationToken);
-		await context.SaveChangesAsync(cancellationToken);
-	}
+        await context.Set<JobState>().AddAsync(jobState, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+    }
 
-	private async static Task UpdateChildJobs(TContext context, string parentJobId, CancellationToken cancellationToken)
-	{
-		await context.Set<Job>()
-			.Where(x => x.ParentJobId == parentJobId || x.ParentBatch.Job.ParentJobId == parentJobId)
-			.Where(x => x.CurrentState == State.Awaiting)
-			// If a job has Batch property in it, then it's a placeholder job, and we don't want to change current status of a placeholder job
-			.Where(x => x.Batch == null)
-			.ExecuteUpdateAsync(x => x.SetProperty(y => y.CurrentState, State.Enqueued), cancellationToken);
-	}
+    private async static Task UpdateChildJobs(TContext context, string parentJobId, CancellationToken cancellationToken)
+    {
+        await context.Set<Job>()
+            .Where(x => x.ParentJobId == parentJobId || x.ParentBatch.Job.ParentJobId == parentJobId)
+            .Where(x => x.CurrentState == State.Awaiting)
+            // If a job has Batch property in it, then it's a placeholder job, and we don't want to change current status of a placeholder job
+            .Where(x => x.Batch == null)
+            .ExecuteUpdateAsync(x => x.SetProperty(y => y.CurrentState, State.Enqueued), cancellationToken);
+    }
 
-	private async static Task UpdateCurrentAndNextBatchFromChildJob(TContext context, string batchId, CancellationToken cancellationToken)
-	{
-		var currentBatch = await context.Set<Batch>()
-			.Where(x => x.Id == batchId)
-			.TagWith(InterceptorConstants.RowLockTableBatch)
-			.FirstOrDefaultAsync(cancellationToken);
+    private async static Task UpdateCurrentAndNextBatchFromChildJob(TContext context, string batchId, CancellationToken cancellationToken)
+    {
+        var currentBatch = await context.Set<Batch>()
+            .Where(x => x.Id == batchId)
+            .TagWith(InterceptorConstants.RowLockTableBatch)
+            .FirstOrDefaultAsync(cancellationToken);
 
-		// Check if this is a batch job
-		if (currentBatch == null)
-		{
-			return;
-		}
+        // Check if this is a batch job
+        if (currentBatch == null)
+        {
+            return;
+        }
 
-		currentBatch.Counter--;
+        currentBatch.Counter--;
 
-		// If all jobs in a single batch are finished
-		if (currentBatch.Counter > 0)
-		{
-			return;
-		}
+        // If all jobs in a single batch are finished
+        if (currentBatch.Counter > 0)
+        {
+            return;
+        }
 
-		currentBatch.Counter = 0;
+        currentBatch.Counter = 0;
 
-		var currentBatchJob = await context.Set<Job>()
-			.Where(x => x.Id == currentBatch.Id)
-			.FirstAsync(cancellationToken);
+        var currentBatchJob = await context.Set<Job>()
+            .Where(x => x.Id == currentBatch.Id)
+            .FirstAsync(cancellationToken);
 
-		currentBatchJob.CurrentState = State.Completed;
+        currentBatchJob.CurrentState = State.Completed;
 
-		var nextBatchJob = await context.Set<Job>()
-			.Where(x => x.ParentJobId == currentBatchJob.Id)
-			.FirstOrDefaultAsync(cancellationToken);
+        var nextBatchJob = await context.Set<Job>()
+            .Where(x => x.ParentJobId == currentBatchJob.Id)
+            .FirstOrDefaultAsync(cancellationToken);
 
-		// Check if another parent job exists
-		// If yes, then start another batch jobs process
-		// if no, then no more jobs exists that need to be started (this is the last one)
-		if (nextBatchJob == null)
-		{
-			return;
-		}
+        // Check if another parent job exists
+        // If yes, then start another batch jobs process
+        // if no, then no more jobs exists that need to be started (this is the last one)
+        if (nextBatchJob == null)
+        {
+            return;
+        }
 
-		var nextBatch = await context.Set<Batch>()
-			.Where(x => x.Id == nextBatchJob.Id)
-			.FirstOrDefaultAsync(cancellationToken);
+        var nextBatch = await context.Set<Batch>()
+            .Where(x => x.Id == nextBatchJob.Id)
+            .FirstOrDefaultAsync(cancellationToken);
 
-		// Check if this is another batch of jobs or...
-		if (nextBatch != null)
-		{
-			var nextBatchJobs = await context.Set<Job>()
-				.Where(x => x.BatchId == nextBatch.Id)
-				.ToListAsync(cancellationToken);
+        // Check if this is another batch of jobs or...
+        if (nextBatch != null)
+        {
+            var nextBatchJobs = await context.Set<Job>()
+                .Where(x => x.BatchId == nextBatch.Id)
+                .ToListAsync(cancellationToken);
 
-			foreach (var batchJob in nextBatchJobs)
-			{
-				batchJob.CurrentState = State.Enqueued;
-			}
-		}
-		// ...A single job
-		else
-		{
-			nextBatchJob.CurrentState = State.Enqueued;
-		}
-	}
+            foreach (var batchJob in nextBatchJobs)
+            {
+                batchJob.CurrentState = State.Enqueued;
+            }
+        }
+        // ...A single job
+        else
+        {
+            nextBatchJob.CurrentState = State.Enqueued;
+        }
+    }
 
-	private class JobData
-	{
-		public Job Job { get; set; } = null!;
+    private class JobData
+    {
+        public Job Job { get; set; } = null!;
 
-		public bool IsParent { get; set; }
-	}
+        public bool IsParent { get; set; }
+    }
 }
