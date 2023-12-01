44eb9f3c7f0bdc99155d12c7c99842255fb83801
karlopeta-moberg <karlo.petonjic@moberg.hr>
2023-12-01 15:53:53 +0100

    add react icons

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -4,17 +4,20 @@ import Batches from "./pages/batches/index";
 import Jobs from "./pages/jobs/index";
 import ReccuringJobs from "./pages/recurring_jobs/index";
 import Navbar from "./components/navbar/Navbar";
-
+import Layout from "./components/layout/Layout";
 const App: React.FC = () => {
   return (
     <Router>
 		<Navbar />
+    <Layout>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/recurring-jobs" element={<ReccuringJobs />} />
         <Route path="/batches" element={<Batches />} />
         <Route path="/jobs" element={<Jobs />} />
-        <Route path="/recurring-jobs" element={<ReccuringJobs />} />
       </Routes>
+    </Layout>
+ 
     </Router>
   );
 };
