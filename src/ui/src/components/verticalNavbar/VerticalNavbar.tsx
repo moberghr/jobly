2e794baa1ec6aa63767ef1f9b55c3b7800d923b8
karlopeta-moberg <karlo.petonjic@moberg.hr>
2023-12-06 10:28:53 +0100

    add styles modules

--- src/ui/src/components/verticalNavbar/VerticalNavbar.tsx
+++ src/ui/src/components/verticalNavbar/VerticalNavbar.tsx
@@ -6,13 +6,13 @@ import { GoPulse } from "react-icons/go";
 const VerticalNavbar = () => {
   return (
     <div className={style.container}>
-      <NavItem label="Enqueued" Icon={FaLayerGroup} quantity={5}/>
-      <NavItem label="Scheduled" Icon={FaCalendarAlt} quantity={215}/>
-      <NavItem label="Processing" Icon={GoPulse} quantity={44125}/>
-      <NavItem label="Succeeded" Icon={FaCheckCircle} quantity={522}/>
-      <NavItem label="Failed" Icon={FaExclamation} quantity={421}/>
-      <NavItem label="Deleted" Icon={FaTrash} quantity={2}/>
-      <NavItem label="Awaiting" Icon={FaClock} quantity={5}/>
+      <NavItem label="Enqueued" Icon={FaLayerGroup} iconColor={"darkblue"} quantity={5}/>
+      <NavItem label="Scheduled" Icon={FaCalendarAlt} iconColor={"darkblue"} quantity={215}/>
+      <NavItem label="Processing" Icon={GoPulse} iconColor={"darkblue"} quantity={44125}/>
+      <NavItem label="Succeeded" Icon={FaCheckCircle} iconColor={"darkblue"} quantity={522}/>
+      <NavItem label="Failed" Icon={FaExclamation} iconColor={"darkblue"} quantity={421}/>
+      <NavItem label="Deleted" Icon={FaTrash} iconColor={"darkblue"} quantity={2}/>
+      <NavItem label="Awaiting" Icon={FaClock} iconColor={"darkblue"} quantity={5}/>
     </div>
   )
 }
