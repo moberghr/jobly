4f12182e944b175e2509046d76abecdd9a354188
matteofalsetti-moberg <matteo.falsetti@moberg.hr>
2024-03-12 11:18:30 +0100

    added import fixes

--- src/ui/src/api/navigation.api.ts
+++ src/ui/src/api/navigation.api.ts
@@ -1,3 +1,8 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+import { IGetNavigationCountResponse } from ".";
+import { API_URL_Mock } from "../utils/constants";
+
 export async function getNavigationData(): Promise<IGetNavigationCountResponse | undefined> {
     const data = await axios
         .get(`${API_URL_Mock}/navigationdata`)
