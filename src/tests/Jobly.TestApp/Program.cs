169f969d23e6e2e5ed8e7a33153bd403df56a22f
Halldór Sigurðsson <halldor@aur.is>
2024-04-06 08:04:48 +0000

    uncomment migrate

--- src/tests/Jobly.TestApp/Program.cs
+++ src/tests/Jobly.TestApp/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddJoblyWorker<TestContext>(options =>
 var app = builder.Build();
 
 // comment after db is created
-// await Migrate();
+await Migrate();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
