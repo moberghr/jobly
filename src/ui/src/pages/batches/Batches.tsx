c276442b66f8b469de9f798878d740dcde3d9e5e
Petra Jović <moberg.petra@mp.is>
2024-05-22 12:58:24 +0200

    Quick fix

--- src/ui/src/pages/batches/Batches.tsx
+++ src/ui/src/pages/batches/Batches.tsx
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom";
-import { useLocation } from "react-router";
 import { useState } from "react";
 
 import styles from "./styles.module.scss";
@@ -21,35 +20,6 @@ export interface IBatch {
 const Batches = () => {
     const { batchType } = useParams();
 
-    const MOCK_DATA = {
-        data: [
-            {
-                id: {
-                    type: "primary",
-                    value: "msmi431pe",
-                    pathId: "msmi431pe",
-                    mainRoute: "batches",
-                },
-                description: `Email Campaign ${Math.floor(Math.random() * 100)}`,
-                nonFinished: "100/200",
-                progress: "50%",
-                created: "8 min ago",
-            },
-            {
-                id: {
-                    value: "a7234daa2",
-                    pathId: "a7234daa2",
-                    mainRoute: "batches",
-                },
-                description: `Email Campaign ${Math.floor(Math.random() * 100)}`,
-                nonFinished: "133/512",
-                progress: "31%",
-                created: "5 hours ago",
-            },
-        ],
-        totalCount: 2,
-    };
-
     const COLUMNS = {
         id: "Id",
         description: "Description",
@@ -57,18 +27,15 @@ const Batches = () => {
         progress: "Progress",
         created: "Created",
     };
-    const { pathname } = useLocation();
-    const status = pathname.split("/").at(2);
 
     const [selectedBatch, setSelectedBatch] = useState<IBatch | null>(null);
 
     return (
         <div className={styles.container}>
-            <JoblyTitle>{`${status} batches`}</JoblyTitle>
+            <JoblyTitle>{`${batchType} batches`}</JoblyTitle>
             <BatchesTable />
         </div>
     );
-    return <div>Batch type is {batchType}</div>;
 };
 
 export default Batches;
